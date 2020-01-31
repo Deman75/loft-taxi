c0110834fec5d92be1fa36ba806ecc523e38136a
Dmitriy Shapiro <deman75@yandex.ru>
2020-01-31 15:42:47 +0300

    Поправил верску компонента Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -9,9 +9,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
+import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -20,16 +18,12 @@ const useStyles = makeStyles(theme => ({
     flexDirection: "column",
     alignItems: "center"
   },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
-  },
   form: {
     width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(0)
   },
   submit: {
-    margin: theme.spacing(3, 0, 0)
+    margin: theme.spacing(5, 0, 0)
   },
   grid: {
     marginRight: "10px"
@@ -64,9 +58,7 @@ function Login(props) {
 
   return (
     <div className="login">
-      <Typography component="h1" variant="h4">
-        Войти
-      </Typography>
+      <h1 style={{ marginLeft: "-2px", marginBottom: "1rem" }}>Войти</h1>
       <form className={classes.form} noValidate>
         <Grid container>
           <Grid item className={classes.grid}>
@@ -79,28 +71,33 @@ function Login(props) {
           </Grid>
         </Grid>
         <Grid container direction="column">
-          <Grid item xs>
-            <InputLabel htmlFor="component-simple">
-              Имя пользователя*
-            </InputLabel>
-            <Input
-              inputRef={inputLogin}
-              type="email"
-              id="email"
-              fullWidth
-              // validate
-            />
-          </Grid>
-          <Grid item xs>
-            <InputLabel htmlFor="component-simple">Пароль*</InputLabel>
-            <Input
-              inputRef={inputPass}
-              type="password"
-              id="password"
-              fullWidth
-            />
-          </Grid>
+          <TextField
+            inputRef={inputLogin}
+            variant="standard"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            inputRef={inputPass}
+            variant="standard"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+
           <Button
+            margin="normal"
             type="submit"
             variant="contained"
             color="primary"
